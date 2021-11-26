d88c6accc96f371652c90390a8de999191c4bc27
David <dtsulik@gmail.com>
2021-11-27 02:22:54 +0400

    add simplest app for probes

---
+++ 03-simple-prober/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/healthz')
+def health(arg):
+    if "FAIL_HEALTH" in os.environ:
+        return "Not OK!", 400
+    return f"OK"
+
+@app.route('/livez')
+def start():
+    if "FAIL_LIVE" in os.environ:
+        return "Not OK!", 400
+    return f"OK"
+
+if __name__ == '__main__':
+    app.run()
